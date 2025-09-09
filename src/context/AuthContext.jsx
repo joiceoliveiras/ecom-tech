59a3310de6b3431e1184ee548ff1f65b21d14a82
Joice Oliveira <joicedeoliveirasantana@gmail.com>
2025-09-09 08:50:19 -0300

    Dia 2: criar AuthContext e configurar AuthProvider no main.jsx

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState, useEffect } from "react";
+
+export const AuthContext = createContext(); 
+
+export function AuthProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+    useEffect(() => {
+    const savedUser = localStorage.getItem("user");
+    if (savedUser) {
+        setUser(JSON.parse(savedUser));
+    }
+   }, []);
+
+   function login(username, password) {
+
+    if (password === "123") {
+        const loggedUser = { username };
+        setUser(loggedUser);
+        localStorage.setItem("user", JSON.stringify(loggedUser));
+        return true;
+    }
+    return false;
+   }
+
+   function logout() {
+    setUser(null);
+    localStorage.removeItem("user");
+   }
+
+   return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+        {children}
+    </AuthContext.Provider>
+   );
+}
