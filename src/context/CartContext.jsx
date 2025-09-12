f283c8d973ff229b52c7faf862b93229227f507b
Joice Oliveira <joicedeoliveirasantana@gmail.com>
2025-09-12 16:29:12 -0300

    feat: criar CartContext com add/remove e persistência

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,37 @@
+// src/context/CartContext.jsx
+import { createContext, useContext, useState, useEffect } from "react";
+
+const CartContext = createContext();
+
+export function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const savedCart = localStorage.getItem("cart");
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
+  function addToCart(product) {
+    setCart((prev) => [...prev, product]);
+  }
+
+  function removeFromCart(id) {
+    setCart((prev) => prev.filter((item) => item.id !== id));
+  }
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  return useContext(CartContext);
+}
