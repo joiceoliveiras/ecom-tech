338f63e478adc99d79601ace97f338132cb9d7bb
Joice Oliveira <joicedeoliveirasantana@gmail.com>
2025-09-17 06:02:41 -0300

    feat(auth): adicionar componente ProtectedRoute para proteger rota

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,12 @@
-import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
+import { useAuth } from "../context/AuthContext";
 
 export default function PrivateRoute({ children }) {
-  const { user } = useContext(AuthContext);
-  return user ? children : <Navigate to="/login" />;
+  const { user } = useAuth();
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
 }
