e821816ee171e5404a9b52326edcf3aed41829f1
Joice Oliveira <joicedeoliveirasantana@gmail.com>
2025-09-17 06:05:34 -0300

    feat(routes): proteger rotas de Produtos e Carrinho usando PrivateRoute

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
-import { Routes, Route} from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Login from './pages/Login'
-import Produtos from './pages/Produtos'
-import Carrinho from './pages/Carrinho'
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Login from "./pages/Login";
+import Produtos from "./pages/Produtos";
+import Carrinho from "./pages/Carrinho";
 import PrivateRoute from "./components/PrivateRoute";
 
-
-export default function App() {
+function App() {
   return (
-     <div className="min-h-screen bg-gray-50">
+    <>
       <Navbar />
-      
-    <Routes>
+      <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/produtos" element={
-          <PrivateRoute>
-          <Produtos />
-          </PrivateRoute>
-          } />
-        <Route path="/carrinho" element={
-          <PrivateRoute>
-          <Carrinho />
-          </PrivateRoute>
-          } />
+
+        <Route
+          path="/produtos"
+          element={
+            <PrivateRoute>
+              <Produtos />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/carrinho"
+          element={
+            <PrivateRoute>
+              <Carrinho />
+            </PrivateRoute>
+          }
+        />
       </Routes>
-    </div>
-  )
+    </>
+  );
 }
+
+export default App;
